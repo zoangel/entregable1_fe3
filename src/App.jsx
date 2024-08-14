be5bc3f383d110234e9923aa9797e3efcf48ef3e
AngelHw <angel.rg@aol.com>
2024-08-14 17:58:55 -0600

    primer publicacion

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./Card";
+
+function App() {
+  return (
+        <div className="App">
+          <h1>Carga de estudiantes</h1>
+          <form></form>
+          <Card />
+        </div>
+  );
+}
+
+export default App;
